38402c58ef7cd9915554561f5c3686495c5afc8c
JHCTDEV <jhct.dev@gmail.com>
2021-12-03 17:54:15 +0000

    init

---
+++ src/main/java/com/jhct/inspeccionespluspetrol/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jhct.inspeccionespluspetrol;
+
+/**
+ *
+ * @author jhctd
+ */
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
